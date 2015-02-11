3f9904e660cc63d67b3d1f88537e83547b7029a0
Tom Fuertes <tomfuertes@gmail.com>
2015-02-10 20:00:55 -0600

    tmp checkin

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*jshint node:true*/
 
 var appname = 'npi'; // .npirc
@@ -19,6 +20,7 @@ program
   .usage('[options] <file ...>')
   .option('-u, --target-url [url]', 'The target url to proxy', 'http://jquery.com/')
   .option('-d, --target-dir [path]', 'The path of the target directory to watch', './')
+  .option('-e, --exclude [pattern]', 'Global Pattern to exclude', undefined)
   .option('-p, --port <n>', 'The proxy port', myParseInt, '8000')
   .option('-o, --open', 'Open a browser window', false)
   .option('-c, --create-rc', 'Create .npirc file', false)
@@ -31,7 +33,21 @@ if (targetUrl.substring(0, 4) !== "http") {
 }
 targetUrl = url.parse(targetUrl);
 
-var targetDir = program.targetDir;
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+var globals = glob.sync('./**/*global*', {
+  cwd: program.targetDir
+});
+var fileGlob = ['./**/*', '!./**/*' + program.exclude + '*'];
+
+// usage example:
+var a = ['a', 1, 'a', 2, '1'];
+var unique = a.filter(onlyUnique); // returns ['a', 1, 2, '1']
+
+var files = glob(fileGlob, {
+  cwd: program.targetDir
+});
 
 function myParseInt(string, defaultValue) {
   var int = parseInt(string, 10);
@@ -52,11 +68,11 @@ var config = { // defaults:
 
 // write rc or prompt w/ if already exist
 if (program.createRc) {
-  var writeConf = function () {
+  var writeConf = function() {
     fs.writeFileSync(rcfile, JSON.stringify(config, 2, ' '));
   };
 
-  if ( ! fs.existsSync(rcfile) ) writeConf();
+  if (!fs.existsSync(rcfile)) writeConf();
   else {
     require('colors');
     var prompt = require('readline-sync');
@@ -65,7 +81,7 @@ if (program.createRc) {
     diff.diffJson(
       JSON.parse(fs.readFileSync(rcfile).toString()),
       config
-    ).forEach(function(part){
+    ).forEach(function(part) {
       // green for additions, red for deletions
       // grey for common parts
       var color = part.added ? 'green' :
